7df3878c4b2aa5f4fefd1a53b440892af9ce8c01
keyurshah01 <keyurshah01@users.noreply.github.com>
2024-08-22 19:48:34 -0400

    Update streamlit_app.py
    added search_on to the dataframe and a stop() command

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,10 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie wil be: ", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients",
     my_dataframe,
